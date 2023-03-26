941f950a741b33394478c7add6f9e7637d9f34cc
laszloekovacs <laszloekovacs@gmail.com>
2023-03-26 21:20:04 +0200

    more styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const client = new QueryClient()
 
 const App = () => {
     return (
-        <div className="w-full mb-40">
+        <div className="w-full mb-40 mt-10">
             <div className="w-11/12 mx-auto">
                 <h1 className="text-xl text-center font-bold mb-5 ">
                     Tailwind CSS styling checklist
