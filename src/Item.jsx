6a92cf92e3fa24f2527e27effd2135e5908fd950
laszloekovacs <laszloekovacs@gmail.com>
2023-03-28 12:45:22 +0200

    added comment

--- src/Item.jsx
+++ src/Item.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 const base = 'https://tailwindcss.com/docs/'
 
+/* renders a "chapter", meaning either a link or a header.
+it renders into a flat list, meaning headers and links are on the same level
+enclosed in <li> tags.
+hovering on a link will show a popup with the table of classes from the tailwind
+reference page.
+*/
 const Item = ({ group }) => {
     return (
         <>
